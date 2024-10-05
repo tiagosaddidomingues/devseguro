26d39422ec734337141dfd4fd948c1cb424333a4
Tiago Saddi Domingues <tiago.saddi@gmail.com>
2024-10-05 12:05:11 -0300

    SQL de criação de banco de dados

---
+++ SQL.sql
@@ -0,0 +1,29 @@
+CREATE TABLE cliente (
+  id_cliente int NOT NULL AUTO_INCREMENT,
+  nome varchar(100) NOT NULL,
+  senha varchar(10) NOT NULL,
+  eMail varchar(50)  NULL,
+  telefone varchar (20) NULL,
+  endereco varchar (100) NULL,
+  
+  PRIMARY KEY (id_cliente)
+)ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE produto (
+  id_produto int NOT NULL AUTO_INCREMENT,
+  nome varchar(100) NOT NULL,
+  descricao varchar(200) NOT NULL,
+  preco Decimal(10,2) NULL,
+  
+  PRIMARY KEY (id_produto)
+)ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE Pedido (
+  id_cliente int NOT NULL,
+  id_produto int NOT NULL,
+  quantidade int NOT NULL,
+  preço_unitario DECIMAL(10,2) NOT NULL,
+  data_compra date NOT NULL,
+  
+  PRIMARY KEY (id_cliente,id_produto)
+)ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
